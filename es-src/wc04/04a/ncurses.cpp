d78131648cfe1835918d5ae71e1e0dab916c9c3c
Tycho <tycho.jobsis@hva.nl>
2022-02-16 09:08:56 +0100

    secend window?

--- es-src/wc04/04a/ncurses.cpp
+++ es-src/wc04/04a/ncurses.cpp
@@ -8,6 +8,7 @@
 int main() {
 
   WINDOW *pWin = NULL;
+  WINDOW *win2 = NULL;
 
   // Start curses mode
   initscr();
@@ -17,12 +18,18 @@ int main() {
   printw("Hello World !!! ");  
 
   // Create a ncurses window
-  pWin = newwin(10, 30, 5, 15);
+  pWin = newwin(5, 25, 0, 10);
   wborder(pWin, '|', '|', '-', '-', '+', '+', '+', '+');
+
+  win2 = newwin(10, 30, 5, 15);
+  wborder(win2, '|', '|', '-', '-', '+', '+', '+', '+');
 
   // Print text in window
   mvwprintw(pWin, 3, 1, "Hello Window :-) ");
   wrefresh(pWin);
+
+  mvwprintw(win2, 3, 1, "Hello Window :-) ");
+  wrefresh(win2);
 
   // Close the program
   getch();
